d6f834dbdd338fdbbc2ba66fd0904bab899d1896
edb-c <edb.cofresi@gmail.com>
2019-06-11 14:35:51 -0400

    add README

---
+++ README.md
@@ -0,0 +1,14 @@
+Customerbase GraphQL Server
+
+Node.js/Express CRUD backend using GraphQL and JSON-Server
+
+$ npm install
+-Run JSON-Server (Port 3000)
+
+$ npm run json:server
+-Run Server (Port 4000)
+
+$ npm run dev:server
+-Visit Graphiql IDE
+
+Go to http://localhost:4000/graphql
--- schema.js
+++ schema.js
@@ -9,6 +9,7 @@ const {
 } = require('graphql');
 
 // Hardcoded data
+
 const customers = [
   { id: '1', name: 'John Doe', email: 'jdoe@gmail.com', age: 35 },
   { id: '2', name: 'Steve Smith', email: 'steve@gmail.com', age: 25 },
@@ -36,12 +37,13 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLString }
       },
       resolve(parentValue, args) {
+        /*
         for (let i = 0; i < customers.length; i++) {
           if (customers[i].id == args.id) {
             return customers[i];
           }
         }
-        //        */
+              */
         return axios
           .get('http://localhost:3000/customers/' + args.id)
           .then(res => res.data);
